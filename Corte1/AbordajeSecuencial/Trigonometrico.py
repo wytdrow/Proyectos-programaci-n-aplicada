e704faccf6563c3bc8150c8ad194c161b5471b91
wytdrow <119146574+wytdrow@users.noreply.github.com>
2025-09-24 18:27:25 -0500

    Create Trigonometrico.py

---
+++ Corte1/AbordajeSecuencial/Trigonometrico.py
@@ -0,0 +1,14 @@
+import math
+print("")
+redians= float(input("Ingrese valor de radieanes"))
+proyeccion= math.sin(redians)
+print("Seno Radianes=",proyeccion)
+print("")
+grados= redians*180/math.pi
+print("Radianes=", redians)
+print("")
+print("Grados=", grados)
+print("")
+proyecciongr= math.radians(grados)
+print("Seno Grados=", proyeccion)
+print("")
